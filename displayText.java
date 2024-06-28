2c82f20734f72a4287394e32329289e5e6fc9cf9
Pramot674259011 <naruapon@gmail.com>
2024-06-28 10:51:29 +0700

    first commit

---
+++ displayText.java
@@ -0,0 +1,12 @@
+public class displayText{
+    public static void main(String[]args){
+     System.out.println("Pramot"+"Uawan");
+     System.out.println(10 +5);
+     System.out.println("NPRN"+2567);
+        System.out.println(0+"3"+2);
+     
+     
+
+    }
+
+}
